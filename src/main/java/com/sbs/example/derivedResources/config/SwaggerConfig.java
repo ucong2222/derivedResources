c94225b824d325dd06585a2a46dbc15ae472f6b0
ucong2222 <dbrudrjf21@gmail.com>
2021-04-02 00:12:34 +0900

    스웨거 메타정보 입력

--- src/main/java/com/sbs/example/derivedResources/config/SwaggerConfig.java
+++ src/main/java/com/sbs/example/derivedResources/config/SwaggerConfig.java
@@ -3,8 +3,11 @@ package com.sbs.example.derivedResources.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,7 +20,13 @@ public class SwaggerConfig {
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
 				.apis(RequestHandlerSelectors.basePackage("com.sbs.example.derivedResources.controller"))
-				.paths(PathSelectors.any()).build();
+				.paths(PathSelectors.any()).build().apiInfo(apiInfo());
+	}
+	
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("이미지 파생 서비스").description("간단한 imgURL을 통해 파생된 이미지를 제공합니다.")
+				.contact(new Contact("제작자", "https://github.com/ucong2222/derivedResources", "dbrudrjf21@gmail.com"))
+				.version("0.1").build();
 	}
 
 }
